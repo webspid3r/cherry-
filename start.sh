b9bfe6960c92ee03bc99ef5aebfc96e71e0e4edd
spid3r <web.spid3rss@gmail.com>
2025-05-22 02:35:02 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,6 @@ echo -e "${RED}Installing dependencies...${NC}"
 python3 -m pip install --upgrade pip
 python3 -m pip install requests colorama
 python3 -m pip install requests requests
-python3 -m pip install requests ctypes
 
 echo -e "${RED}Starting main.py...${NC}"
 python3 main.py
